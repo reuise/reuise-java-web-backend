5da4bf46d93f1c5c101604cdd00e25a5568b1e86
Damo <damien@spatialmedia.com.au>
2021-09-24 17:25:23 +1000

    Update

---
+++ AbstractWebBackendButton.java
@@ -0,0 +1,4 @@
+package dev.reuise.web.backend;
+
+public class AbstractWebBackendButton extends dev.reuise.web.core.AbstractWebButton{
+}
---
+++ Button.java
@@ -0,0 +1,4 @@
+package dev.reuise.web.backend;
+
+public class Button extends AbstractWebBackendButton {
+}
